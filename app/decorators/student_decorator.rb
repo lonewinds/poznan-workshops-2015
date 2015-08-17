eb9027d3d18840138c46ddd0ab8756decc73d54e
Marcin Jakubowski <maarcinjak@gmail.com>
2015-08-17 17:45:32 +0200

    Decorators tests done
    Changes: rename of 'subject_item_decorator.rb' to 'subject_item'decorator_spec.rb',
    add implementation of 'avg_notes' method in 'student_decorator.rb',
    add implementation of 'full_name_with_title' method in 'teacher_decorator.rb',
    fixed 'build :teacher' (before -> build :student) in 'student_decorator_spec.rb'

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,11 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = Array.new
+    subject_item.subject_item_notes.each do |note|
+      notes.push note.value
+    end
+
+    avg = notes.size == 0 ? sprintf('%.2f', 0) : sprintf('%.2f', notes.inject{ |sum, el| sum + el }.to_f / notes.size)
   end
 end
