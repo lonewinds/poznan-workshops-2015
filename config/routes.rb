09f7dbd6ca1dab88fe9cceef19d444c69c7da6dc
Marcin Jakubowski <maarcinjak@gmail.com>
2015-08-17 17:15:30 +0200

    Controller's tests done
    Added: some missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :students do
     get :subjects
   end
+  get '/reports' => 'reports#subjects'
+  resources :teachers
+  resources :visitors, only: [:index]
 end
